02f3ec1a7040597144e398cfa529f6422b8ede6e
Souraevshing <isauravshing@gmail.com>
2024-02-12 23:36:06 +0530

    add: query-string lib, debounce complete

--- app/(dashboard)/_components/search-input/index.tsx
+++ app/(dashboard)/_components/search-input/index.tsx
@@ -1,6 +1,44 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useEffect, useState } from "react";
+import qs from "query-string";
+import { useDebounce, useDebounceValue } from "usehooks-ts";
+
+import { SearchIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
 //search input to search organizations
 export const SearchInput = () => {
-  return <div>index</div>;
+  const router = useRouter();
+  const [value, setValue] = useState("");
+
+  //debounce the search value to delay for 400ms
+  const debounceValue = useDebounceValue(value, 400);
+
+  const [debounceSearchValue] = debounceValue;
+
+  useEffect(() => {
+    //constructing url based on query passed inside search box
+    const URL = qs.stringifyUrl(
+      { url: "/", query: { search: debounceSearchValue } },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(URL);
+  }, [debounceValue, router]);
+
+  const handleChange = () => {};
+
+  return (
+    <div className="w-full relative">
+      <SearchIcon />
+      <Input
+        placeholder="Search organization"
+        className="w-full max-w-[516px] pl-9"
+        onChange={handleChange}
+        value={value}
+      />
+    </div>
+  );
 };
